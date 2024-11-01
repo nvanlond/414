aaab1b92ca8ea630074b2cc9faa11563e154655b
nvanlond <nvanlond@terpmail.umd.edu>
2024-10-31 22:42:37 -0400

    Add files via upload

--- Module 2/cleaningleaders.py
+++ Module 2/cleaningleaders.py
@@ -4,12 +4,10 @@ import pandas as pd
 file_path = '/Users/niels/code/414/module 2/world data.csv'  
 leaders_df = pd.read_csv(file_path)
 
-# Drop the second column (flag) by specifying its index
+# Drop the second column (flag)
 leaders_df.drop(leaders_df.columns[6], axis=1, inplace=True)
 leaders_df.drop(leaders_df.columns[0], axis=1, inplace=True)
 
-# Display the DataFrame to verify the column is removed
 print(leaders_df.head())
 
-# If you want to save the updated DataFrame back to a CSV file
 leaders_df.to_csv('cleaned_leaders_data.csv', index=False)
